6134f0ec24f2515c21a2626754b7f0c225a911bf
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-22 15:03:33 -0300

    upload photo

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const formidable = require('formidable');
+const fs = require('fs');
 const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
@@ -37,19 +39,51 @@ exports.getUser = (req, res) => {
     return res.json(req.profile);
 };
 
+// exports.updateUser = (req, res) => {
+//     let user = req.profile;
+//     user = _.extend(user, req.body);
+//     user.updated = Date.now();
+//     user.save((err) => {
+//         if (err) {
+//             return res
+//                 .status(400)
+//                 .json({ error: 'You are not authorized to update this user' });
+//         }
+//         user.hashed_password = undefined;
+//         user.salt = undefined;
+//         res.json(user);
+//     });
+// };
+
 exports.updateUser = (req, res) => {
-    let user = req.profile;
-    user = _.extend(user, req.body);
-    user.updated = Date.now();
-    user.save((err) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
         if (err) {
-            return res
-                .status(400)
-                .json({ error: 'You are not authorized to update this user' });
+            console.log({ err });
+            return res.status(400).json({
+                error: 'Photo could not be uploaded',
+            });
         }
-        user.hashed_password = undefined;
-        user.salt = undefined;
-        res.json(user);
+        let user = req.profile;
+        user = _.extend(user, fields);
+        user.updated = Date.now();
+
+        if (files.photo) {
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err,
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        });
     });
 };
 
